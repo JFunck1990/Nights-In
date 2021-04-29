a8ee61bebb8de3587899a42b2bc980596109f70a
JP-Grace <gracejohnpaul200@gmail.com>
2021-04-28 20:48:42 -0400

    dashboard translated from handlebars to react

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,7 +2,21 @@ import React from "react";
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div id="main-container" className="container">
+      <div className="jumbotron text-center header-wrap" align="center">
+        <p className="lead">Welcome to <strong>Sample App</strong></p>
+        <hr className="my-4" />
+        <div>
+          {/* {{#if isloggedin}} */}
+            {/* You are logged in as {{user.firstName}} */}
+            <a href="/logout"> <button type="btn" className="btn">Logout</button></a>
+          {/* {{else}} */}
+            <p>You need to log in to use this app.</p>
+          {/* {{/if}} */}
+          <h5 id="login-err-msg"></h5>
+        </div>
+      </div>
+    </div>
   );
 }
 

2666ef255442e07c30fb56173e4219f4f57c3418
Zach Smith <zsmith4331@gmail.com>
2021-05-05 20:25:50 -0400

    fixed bootstrap bug, and update profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -2,89 +2,95 @@ import React from "react";
 
 function Profile() {
   return (
-    <div class="container">
-  <header align="center" class="header-wrap">
-    <h3>View or update your user</h3>
-    <div class="userId">
-      <h3>Welcome, 
-        {/* {{userInfo.firstName}} */}
-        </h3>
-      <h5 id="user-number" data-useremail="{{userInfo.email}}">Account email: 
-      {/* {{userInfo.email}} */}
-      </h5>
-    </div>
-    <h3 id="update-err-msg"></h3>
-  </header>
+    <div class="container pt-5">
 
-  <form id="user-form">
-    <div class="form-row">
-      <div class="form-group col-md-6">
-        <label for="inputFirst">First Name</label>
-        <input type="text" class="form-control" id="inputFirst" value="{{userInfo.firstName}}" placeholder="John"></input>
-      </div>
-      <div class="form-group col-md-6">
-        <label for="inputLast">Last Name</label>
-        <input type="text" class="form-control" id="inputLast" value="{{userInfo.lastName}}" placeholder="Doe"></input>
-      </div>
-    </div>
+      {/* Card Header */}
+      <div className="card border border-dark">
+        <div className="card-header text-center bg-warning">
+          <h2>Update Profile</h2>
+        </div>
+
+      {/* Welcome */}
+        <div className="userId text-center">
+          <h3>Welcome, 
+            {/* {{userInfo.firstName}} */}
+            </h3>
+          <h5 id="user-number" data-useremail="{{userInfo.email}}">Email: 
+          {/* {{userInfo.email}} */}
+          </h5>
+        </div>
+
+        <h3 id="update-err-msg"></h3>
+     
+      <form id="user-form">
+
+      {/* First & Last Name */}
+        <div className="form-row p-2">
+
+          <div className="form-group col-md-6">
+            <label for="inputFirst">First Name</label>
+            <input type="text" class="form-control" id="inputFirst" value="{{userInfo.firstName}}" placeholder="John"></input>
+          </div>
+
+          <div className="form-group col-md-6">
+            <label for="inputLast">Last Name</label>
+            <input type="text" class="form-control" id="inputLast" value="{{userInfo.lastName}}" placeholder="Doe"></input>
+          </div>
+        </div>
   
-
-    <div class="form-row">
-      <div class="form-group col-md-6">
-        <label for="inputPassword">Email</label>
-        <input type="text" class="form-control" id="inputEmail" value="{{userInfo.email}}"></input>
-      </div>
-      <div class="form-group col-md-6">
-        <label for="inputPassword">Password</label>
-        <input type="password" class="form-control" id="inputPassword" value="{{userInfo.password}}"></input>
-      </div>
-    </div>
-
-    <div>
-      <button type="submit" id="update-user" data-id="{{userInfo.id}}" class="btn btn-primary">Update</button>
-
-      {/* {{#unless userInfo.isAdmin}} */}
-
-        <button type="submit" id="delete-user" class="btn btn-primary">Delete</button>
-
-      {/* {{/unless}} */}
-
-      <button type="submit" id="view-user" data-id="{{userInfo.id}}" class="btn btn-primary">Refresh</button>
-    </div>
-  </form>
-
-  <div class="modal" id="delete-user-modal" tabindex="-1" role="dialog">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Re-Enter Credentials to delete user</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span aria-hidden="true">&times;</span>
-          </button>
+      {/* Email & Password */}
+        <div className="form-row p-2">
+          <div className="form-group col-md-6">
+            <label for="inputPassword">Email</label>
+            <input type="text" class="form-control" id="inputEmail" value="{{userInfo.email}}"></input>
+          </div>
+          <div className="form-group col-md-6">
+            <label for="inputPassword">Password</label>
+            <input type="password" class="form-control" id="inputPassword" value="{{userInfo.password}}"></input>
+          </div>
         </div>
 
-        <div class="modal-body">
-          <h3></h3>
-          <form>
-            <div class="form-group">
-              <label for="userEmail">Email:</label>
-              <input type="text" class="form-control" id="userEmail"></input>
-            </div>
-            <div class="form-group">
-              <label for="userPassword">Password:</label>
-              <input type="password" class="form-control" id="userPassword"></input>
-            </div>
-          </form>
+      {/* Buttons */}
+        <div className="text-center pb-4">
+          <button type="submit" id="update-user" data-id="{{userInfo.id}}" className="btn btn-primary mr-3">Update</button>
+          <button type="submit" id="delete-user" className="btn btn-primary mr-3">Delete</button>     
+          <button type="submit" id="view-user" data-id="{{userInfo.id}}" className="btn btn-primary">Refresh</button>
         </div>
 
-        <div class="modal-footer">
-          <button id="confirm-delete" type="button" data-id="{{userInfo.id}}" class="btn btn-primary">Confirm Delete</button>
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">Nah I'll stay</button>
+      </form>
+
+      <div className="modal" id="delete-user-modal" tabindex="-1" role="dialog">
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Re-Enter Credentials to delete user</h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            </div>
+
+            <div className="modal-body">
+              <h3>Sign In:</h3>
+                <form>
+                  <div className="form-group">
+                    <label for="userEmail">Email:</label>
+                      <input type="text" className="form-control" id="userEmail"></input>
+                  </div>
+                  <div className="form-group">
+                    <label for="userPassword">Password:</label>
+                    <input type="password" className="form-control" id="userPassword"></input>
+                  </div>
+                </form>
+            </div>
+
+            <div className="modal-footer">
+              <button id="confirm-delete" type="button" data-id="{{userInfo.id}}" className="btn btn-primary">Confirm Delete</button>
+              <button type="button" className="btn btn-secondary" data-dismiss="modal">Nah I'll stay</button>
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 )};
 
 

eb05e1c21ee6bda6141ea8fdc361ee08fd727ab4
JP-Grace <gracejohnpaul200@gmail.com>
2021-05-06 03:18:32 -0400

    changed database in config.js to nights_in_users; deleted example.js from models; removed reference to example.js from user.js

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
     username: process.env.SEQUELIZE_USER,
     password: process.env.SEQUELIZE_PASSWORD,
-    database: 'project2_dev',
+    database: 'nights_in_users',
     dialect: 'mysql',
     host: process.env.SEQUELIZE_HOST,
     port: 3306
@@ -10,7 +10,7 @@ module.exports = {
   test: {
     username: process.env.TU,
     password: process.env.TP || null,
-    database: 'project2_test',
+    database: 'nights_in_users',
     host: 'localhost',
     port: 3306,
     dialect: 'mysql',
--- server/models/user.js
+++ server/models/user.js
@@ -42,12 +42,6 @@ module.exports = function (sequelize, DataTypes) {
     }
   });
 
-  User.associate = function (models) {
-    User.hasMany(models.Example, {
-      onDelete: 'cascade'
-    });
-  };
-
   // This will check if an unhashed password can be compared to the hashed password stored in our database
   User.prototype.validPassword = function (password) {
     return bcrypt.compareSync(password, this.password);

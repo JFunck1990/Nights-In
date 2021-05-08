dde852115109b6531b848a5a814c13f3f3cc39d1
Matt Randolph <mttrndlph@gmail.com>
2021-05-08 13:29:57 -0400

    nav bar link styling

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,2 +1,5 @@
-drop database if exists nights_in;
-create database nights_in;
+drop database if exists nights_in_users;
+create database nights_in_users;
+use mysql;
+ALTER USER 'root'@‘localhost’ IDENTIFIED WITH mysql_native_password BY 'rootqwer';
+flush privileges;

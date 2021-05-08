aabd4ceae48676ddb4a873352687543a542e4ef6
Jack Funck <jackfunck21@gmail.com>
2021-05-08 10:42:14 -0400

    add thing for deployment

--- server/models/index.js
+++ server/models/index.js
@@ -10,8 +10,10 @@ const db = {};
 let sequelize;
 
 if (config.use_env_variable) {
+  console.log(config);
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
+  console.log(config);
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 

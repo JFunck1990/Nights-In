201efedb4cfd5378d5d0a895d00b5220273a1ae2
Jack Funck <jackfunck21@gmail.com>
2021-05-10 21:21:59 -0400

    Timer redirect

--- client/src/components/Timer/index.js
+++ client/src/components/Timer/index.js
@@ -1,20 +1,35 @@
 
 import React, {useState, useRef, useEffect} from 'react';
+import Scores from "../../pages/Scores/index"
+import { useHistory } from "react-router-dom";
 
 const Timer = (props) => {
+  let history = useHistory();
   const [time, setTime] = useState(new Date().toLocaleTimeString());
-  const secondsPassed = useRef(0);
+  const secondsPassed = useRef(5);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       const date = new Date()
-      secondsPassed.current = secondsPassed.current + 1;
+      secondsPassed.current = secondsPassed.current - 1;
       setTime(date.toLocaleTimeString());
     }, 1000);
     return () => {
       clearTimeout(timeout);
+      // look up react router redirect
+      if(secondsPassed.current <= 0){
+
+        history.push("/scores")
+
+
+      }
     }
+
   }, [time]);
+
+
+
+
 
   return (
     <div>
